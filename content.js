20725810247c3eead170abc24b34d6970b48fd5e
dmitry <dmitry.kouznetsov@paxport.se>
2019-11-18 22:26:39 +0100

    styles

--- content.js
+++ content.js
@@ -1,5 +1,5 @@
 var div=document.createElement("div");
 div.id="overlay";
 
+div.insertAdjacentHTML('afterbegin', "<span class='class123'>test123</span>");
 document.body.appendChild(div);
-div.innerText="test123";
